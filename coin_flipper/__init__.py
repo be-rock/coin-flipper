fc77dccd03c4b0bc01734809acf39d8034add179
brock <brock@pop-os.localdomain>
2021-02-10 12:19:41 -0600

    temporarily remove env tests

--- coin_flipper/__init__.py
+++ coin_flipper/__init__.py
@@ -1,4 +1,3 @@
-from .settings import APP_ENV, get_app_config, get_logger, set_app_env_vars
+from .settings import APP_ENV, get_app_config, get_logger
 
-set_app_env_vars()
 CONFIG = get_app_config()
